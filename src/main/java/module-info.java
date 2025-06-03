efef364fa38f0e07b13f422e164af811a97455b7
setayesh nejadmohammad <105065614+setayesh-nejadmohammad@users.noreply.github.com>
2025-06-03 17:23:13 +0330

    upload files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module game.plantsvszambies {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens game.plantsvszambies to javafx.fxml;
+    exports game.plantsvszambies;
+}
