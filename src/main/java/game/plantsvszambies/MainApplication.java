0d7c597dc9138d7c97096a65c062f28766fa8cbb
setayesh nejadmohammad <nejadmohammad.setayesh@gmail.com>
2025-06-08 15:26:14 +0330

    add card section and put cards on the yard

--- src/main/java/game/plantsvszambies/MainApplication.java
+++ src/main/java/game/plantsvszambies/MainApplication.java
@@ -3,8 +3,11 @@ package game.plantsvszambies;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Cell;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -50,14 +53,87 @@ public class MainApplication extends Application {
             }
         }
 
+
         VBox vbox = new VBox();
+        borderPane.setLeft(vbox);
 
 
         Scene scene = new Scene(borderPane, 1024, 626);
         stage.setTitle("Map");
         stage.setScene(scene);
         stage.show();
+
+        Image sunflowerCard = new Image(getClass().getResourceAsStream("images/Cards/sunflowerCard.png"));
+        ImageView sunflowerCardView = new ImageView(sunflowerCard);
+        sunflowerCardView.setFitWidth(CELL_SIZE*2);
+        sunflowerCardView.setFitHeight(CELL_SIZE*2/1.5);
+        Image peashooterCard = new Image(getClass().getResourceAsStream("images/Cards/peashooterCard.png"));
+        ImageView peashooterCardView = new ImageView(peashooterCard);
+        peashooterCardView.setFitWidth(CELL_SIZE*2);
+        peashooterCardView.setFitHeight(CELL_SIZE*2/1.5);
+
+        Button sunFlowerButton = new Button();
+        Button peashooterButton = new Button();
+
+        sunFlowerButton.setGraphic(sunflowerCardView);
+        peashooterButton.setGraphic(peashooterCardView);
+
+        sunFlowerButton.setOnAction(event -> {
+            for (int row = 0; row < rowNum; row++) {
+                for (int col = 0; col < colNum; col++) {
+                    StackPane cell = createCell(1);
+                    grid.add(cell, col, row);
+                }
+            }
+        });
+
+        peashooterButton.setOnAction(event -> {
+            for (int row = 0; row < rowNum; row++) {
+                for (int col = 0; col < colNum; col++) {
+                    StackPane cell = createCell(2);
+                    grid.add(cell, col, row);
+                }
+            }
+        });
+
+
+        vbox.getChildren().add(sunFlowerButton);
+        vbox.getChildren().add(peashooterButton);
+
     }
+
+    private StackPane createCell(int num) {
+        Rectangle rect = new Rectangle(CELL_SIZE, CELL_SIZE);
+        rect.setFill(null);
+        rect.setStroke(Color.BLACK);
+
+
+        Image sunflower = new Image(getClass().getResourceAsStream("images/Plants/sunflower.gif"));
+        ImageView sunflowerView = new ImageView(sunflower);
+
+        Image peashooter = new Image(getClass().getResourceAsStream("images/Plants/peashooter.gif"));
+        ImageView peashooterView = new ImageView(peashooter);
+
+
+        final StackPane cell = new StackPane();
+        boolean[] isClicked = {false};
+
+        cell.setOnMouseClicked((MouseEvent e) -> {
+            isClicked[0] = true;
+            System.out.println("salmammmmmmm");
+            if(num == 1){
+                cell.getChildren().addAll(sunflowerView);
+            }
+            else if(num == 2){
+                cell.getChildren().addAll(peashooterView);
+            }
+
+        });
+
+
+        return cell;
+    }
+
 
     public static void main(String[] args) {
         launch();
