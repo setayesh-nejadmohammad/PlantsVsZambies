80da90c4354c012f07ccea154970e3958e9abb53
parham <pjohns20202021@gmail.com>
2025-06-08 12:45:11 +0330

    salam

--- src/main/java/game/plantsvszambies/MainApplication.java
+++ src/main/java/game/plantsvszambies/MainApplication.java
@@ -3,6 +3,7 @@ package game.plantsvszambies;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,8 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
