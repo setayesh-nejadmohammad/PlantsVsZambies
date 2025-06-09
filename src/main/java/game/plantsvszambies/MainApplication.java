36dde226f0d45c97304e9c014486881f3fbb63be
setayesh nejadmohammad <105065614+setayesh-nejadmohammad@users.noreply.github.com>
2025-06-09 23:32:08 +0330

    Update MainApplication.java

--- src/main/java/game/plantsvszambies/MainApplication.java
+++ src/main/java/game/plantsvszambies/MainApplication.java
@@ -1,6 +1,7 @@
 package game.plantsvszambies;
 
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,122 +15,114 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainApplication extends Application {
     public static int rowNum = 5;
     public static int colNum = 9;
     public static int CELL_SIZE = 81;
+    private static AtomicInteger num = new AtomicInteger(0);
     @Override
     public void start(Stage stage) throws IOException {
+        Game game = new Game(stage);
+        game.startGame();
 
-        Image frontYard = new Image(getClass().getResourceAsStream("images/frontyard.png"));
-        BorderPane borderPane = new BorderPane();
-        BackgroundImage bgImage = new BackgroundImage(
-                frontYard,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundPosition.DEFAULT,
-                new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true)
-        );
-        borderPane.setBackground(new Background(bgImage));
+        // Image frontYard = new Image(getClass().getResourceAsStream("images/frontyard.png"));
+        // BorderPane borderPane = new BorderPane();
+        // BackgroundImage bgImage = new BackgroundImage(
+        //         frontYard,
+        //         BackgroundRepeat.NO_REPEAT,
+        //         BackgroundRepeat.NO_REPEAT,
+        //         BackgroundPosition.DEFAULT,
+        //         new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true)
+        // );
+        // borderPane.setBackground(new Background(bgImage));
 
-        GridPane grid = new GridPane();
-        grid.setHgap(0); // Remove horizontal gap
-        grid.setVgap(0); // Remove vertical gap
+        // GridPane grid = new GridPane();
+        // grid.setHgap(0); // Remove horizontal gap
+        // grid.setVgap(0); // Remove vertical gap
 
-        grid.setLayoutX(250);
-        grid.setLayoutY(85);
+        // grid.setLayoutX(250);
+        // grid.setLayoutY(85);
 
-        borderPane.getChildren().add(grid);
+        // borderPane.getChildren().add(grid);
 
-        // Create 5x9 grid of squares
-        for (int row = 0; row < rowNum; row++) {
-            for (int col = 0; col < colNum; col++) {
-                Rectangle rect = new Rectangle(CELL_SIZE, CELL_SIZE+10);
-                rect.setFill(null);
-                rect.setStroke(Color.BLACK);
-                rect.setStrokeWidth(0.5);
-                grid.add(rect, col, row);
-            }
-        }
+        // // Create 5x9 grid of squares
+        // for (int row = 0; row < rowNum; row++) {
+        //     for (int col = 0; col < colNum; col++) {
+        //         Rectangle rect = new Rectangle(CELL_SIZE, CELL_SIZE+10);
+        //         rect.setFill(null);
+        //         rect.setStroke(Color.BLACK);
+        //         rect.setStrokeWidth(0.5);
+        //         grid.add(rect, col, row);
+        //         StackPane cell = createCell();
 
-
-        VBox vbox = new VBox();
-        borderPane.setLeft(vbox);
-
-
-        Scene scene = new Scene(borderPane, 1024, 626);
-        stage.setTitle("Map");
-        stage.setScene(scene);
-        stage.show();
-
-        Image sunflowerCard = new Image(getClass().getResourceAsStream("images/Cards/sunflowerCard.png"));
-        ImageView sunflowerCardView = new ImageView(sunflowerCard);
-        sunflowerCardView.setFitWidth(CELL_SIZE*2);
-        sunflowerCardView.setFitHeight(CELL_SIZE*2/1.5);
-        Image peashooterCard = new Image(getClass().getResourceAsStream("images/Cards/peashooterCard.png"));
-        ImageView peashooterCardView = new ImageView(peashooterCard);
-        peashooterCardView.setFitWidth(CELL_SIZE*2);
-        peashooterCardView.setFitHeight(CELL_SIZE*2/1.5);
-
-        Button sunFlowerButton = new Button();
-        Button peashooterButton = new Button();
-
-        sunFlowerButton.setGraphic(sunflowerCardView);
-        peashooterButton.setGraphic(peashooterCardView);
-
-        sunFlowerButton.setOnAction(event -> {
-            for (int row = 0; row < rowNum; row++) {
-                for (int col = 0; col < colNum; col++) {
-                    StackPane cell = createCell(1);
-                    grid.add(cell, col, row);
-                }
-            }
-        });
-
-        peashooterButton.setOnAction(event -> {
-            for (int row = 0; row < rowNum; row++) {
-                for (int col = 0; col < colNum; col++) {
-                    StackPane cell = createCell(2);
-                    grid.add(cell, col, row);
-                }
-            }
-        });
+        //         grid.add(cell,col, row);
+        //     }
+        // }
 
 
-        vbox.getChildren().add(sunFlowerButton);
-        vbox.getChildren().add(peashooterButton);
+
+        // VBox vbox = new VBox();
+        // borderPane.setLeft(vbox);
+
+
+//         Scene scene = new Scene(borderPane, 1024, 626);
+//         stage.setTitle("Map");
+//         stage.setScene(scene);
+//         stage.show();
+
+        // Image sunflowerCard = new Image(getClass().getResourceAsStream("images/Cards/sunflowerCard.png"));
+        // ImageView sunflowerCardView = new ImageView(sunflowerCard);
+        // sunflowerCardView.setFitWidth(CELL_SIZE*2);
+        // sunflowerCardView.setFitHeight(CELL_SIZE*2/1.5);
+        // Image peashooterCard = new Image(getClass().getResourceAsStream("images/Cards/peashooterCard.png"));
+        // ImageView peashooterCardView = new ImageView(peashooterCard);
+        // peashooterCardView.setFitWidth(CELL_SIZE*2);
+        // peashooterCardView.setFitHeight(CELL_SIZE*2/1.5);
+
+        // Button sunFlowerButton = new Button();
+        // Button peashooterButton = new Button();
+
+        // sunFlowerButton.setGraphic(sunflowerCardView);
+        // peashooterButton.setGraphic(peashooterCardView);
+
+        // sunFlowerButton.setOnAction(event -> {
+        //     num.set(1);
+
+        // });
+
+        // peashooterButton.setOnAction(event -> {
+        //     num.set(2);
+        // });
+
+
+        // vbox.getChildren().add(sunFlowerButton);
+        // vbox.getChildren().add(peashooterButton);
+
 
     }
 
-    private StackPane createCell(int num) {
-        Rectangle rect = new Rectangle(CELL_SIZE, CELL_SIZE);
-        rect.setFill(null);
-        rect.setStroke(Color.BLACK);
-
+    private StackPane createCell(){
 
         Image sunflower = new Image(getClass().getResourceAsStream("images/Plants/sunflower.gif"));
         ImageView sunflowerView = new ImageView(sunflower);
-
         Image peashooter = new Image(getClass().getResourceAsStream("images/Plants/peashooter.gif"));
         ImageView peashooterView = new ImageView(peashooter);
-
-
-        final StackPane cell = new StackPane();
-        boolean[] isClicked = {false};
-
+        StackPane cell = new StackPane();
         cell.setOnMouseClicked((MouseEvent e) -> {
-            isClicked[0] = true;
             System.out.println("salmammmmmmm");
-            if(num == 1){
+            if(num.intValue() == 1){
+                num.set(0);
                 cell.getChildren().addAll(sunflowerView);
+
             }
-            else if(num == 2){
+            else if(num.intValue() == 2){
+                num.set(0);
                 cell.getChildren().addAll(peashooterView);
             }
 
         });
-
 
         return cell;
     }
