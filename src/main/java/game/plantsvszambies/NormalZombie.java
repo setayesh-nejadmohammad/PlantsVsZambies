fb1e7bc810c827fc3417c5d605949c8c0de36c76
setayesh nejadmohammad <105065614+setayesh-nejadmohammad@users.noreply.github.com>
2025-06-12 21:32:12 +0330

    Update NormalZombie.java

--- src/main/java/game/plantsvszambies/NormalZombie.java
+++ src/main/java/game/plantsvszambies/NormalZombie.java
@@ -6,12 +6,20 @@ import javafx.scene.image.ImageView;
 public class NormalZombie extends Zombie{
     private Image normalZombieImage = new Image(getClass().getResourceAsStream("images/Zombie/normalzombie.gif"));
 
-    public NormalZombie(int row, int col, Map map) {
-        super(row, col, map, 5, 1);
+    public NormalZombie(int row) {
+        super(100, 50, 0.25, row);
     }
 
     public ImageView getImageView() {
         ImageView imageView = new ImageView(normalZombieImage);
         return imageView;
+    }
+
+    @Override
+    protected ImageView createImageView() {
+        ImageView view = new ImageView(new Image(getClass().getResourceAsStream("images/Zombie/normalzombie.gif")));
+        view.setFitWidth(100);
+        view.setFitHeight(100);
+        return view;
     }
 }
