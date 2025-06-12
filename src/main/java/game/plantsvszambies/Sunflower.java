347212c82a789f9cf66eb49f81ffb0c237f1b535
ParhamMT <77637013+ParhamMT@users.noreply.github.com>
2025-06-12 17:07:30 +0330

    Update Sunflower.java

--- src/main/java/game/plantsvszambies/Sunflower.java
+++ src/main/java/game/plantsvszambies/Sunflower.java
@@ -14,17 +14,13 @@ public class Sunflower extends Plant {
         scheduler.scheduleAtFixedRate(() -> {
             //Sun sun = new Sun(row, col);
             System.out.println("Sun produced at (" + row + "," + col + ")");
-            // اضافه‌کردن sun به مدل بازی
+            
         }, 0, 6, TimeUnit.SECONDS);
     }
 
-    /*@Override
-    public void act(Map map) {
-        // Sunflower عمل مستقیم ندارد، ولی act را می‌توان برای انیمیشن استفاده کرد.
-    }*/
+    
 
     public void stop() {
         scheduler.shutdown();
     }
 }
-
