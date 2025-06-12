e18cc00c67f21a17fe61c8bb966a1d45844e4eed
setayesh nejadmohammad <105065614+setayesh-nejadmohammad@users.noreply.github.com>
2025-06-12 21:20:39 +0330

    Update Zombie.java

--- src/main/java/game/plantsvszambies/Zombie.java
+++ src/main/java/game/plantsvszambies/Zombie.java
@@ -2,109 +2,60 @@ package game.plantsvszambies;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.util.Duration;
-import javafx.animation.TranslateTransition;
-import javafx.application.Platform;
 
-public abstract class Zombie extends Thread {
-    protected int row;
-    protected int col;
+// Base Zombie class
+public abstract class Zombie {
     protected int health;
-    protected int speed; // واحد حرکت در ثانیه
-    protected boolean alive = true;
-    protected Map map;
-    Image image = new Image(getClass().getResourceAsStream("images/Zombie/normalzombie.gif"));
-    ImageView imageView = new ImageView(image);
-    int CELL_SIZE = map.CELL_SIZE;
+    protected int damage;
+    protected double speed; // cells per second
+    protected int row;
+    protected double column; // Using double for smooth movement
+    protected ImageView view;
+    protected boolean isEating = false;
 
-    public Zombie(int row, int col, Map map, int health, int speed) {
-        this.row = row;
-        this.col = col;
+    public Zombie(int health, int damage, double speed, int row) {
         this.health = health;
+        this.damage = damage;
         this.speed = speed;
-        this.map = map;
+        this.row = row;
+        this.column = 9.3; // Starts at rightmost column
+        this.view = createImageView();
     }
 
+    protected abstract ImageView createImageView();
 
-    @Override
-    public void run() {
-        while (alive && col > 0) {
-            try {
-                Thread.sleep(1500); // حرکت در هر ثانیه
-
-                int previousCol = col;
-                col--; // حرکت به چپ
-
-                Platform.runLater(() -> {
-                    // حذف از خانه قبلی و افزودن به خانه جدید
-                    GridPane grid = map.getGridPane();
-
-                    // حذف از موقعیت قبلی
-                    grid.getChildren().remove(imageView);
-
-                    // اضافه به موقعیت جدید
-                    grid.add(imageView, col, row);
-                });
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    public void update(double deltaTime) {
+        if (!isEating) {
+            // Move left
+            column -= speed * deltaTime;
+            view.setLayoutX(column * 80);
         }
-        
-        // Platform.runLater(() -> map.getStackPane(row, col).getChildren().add(imageView));
+    }
 
-        // while (alive && col > 0) {
-        //     try {
-        //         Thread.sleep(2000); // تاخیر بین حرکات
+//    public void attack(Plant plant) {
+//        isEating = true;
+//        plant.takeDamage(damage);
+//        // Play eating animation
+//    }
 
-        //         int prevCol = col;
-        //         int newCol = col - 1;
-        //         col = newCol;
-
-        //         Platform.runLater(() -> {
-        //             TranslateTransition transition = new TranslateTransition(Duration.seconds(1), imageView);
-        //             transition.setByX(-CELL_SIZE); // حرکت به چپ یک خانه
-        //             transition.setOnFinished(e -> {
-        //                 // بعد از حرکت، موقعیت منطقی زامبی آپدیت شده (col--) است
-        //                 // می‌تونی اینجا برخورد یا بررسی جدید انجام بدی
-        //             });
-        //             transition.play();
-        //         });
-
-        //     } catch (InterruptedException e) {
-        //         e.printStackTrace();
-        //     }
-        // }
-
-        // while (alive && col > 0) {
-        //     try {
-        //         Thread.sleep(1000 / speed);
-        //         Cell cell = map.getCell(row, col);
-        //         Plant plant = cell.getPlant();
-        //         map.moveZombieForward(this);
-        //         if (plant != null) {
-        //             plant.takeDamage(1);
-        //             if (plant.isDead()) cell.removePlant();
-        //         } else {
-        //             map.moveZombieForward(this);
-        //         }
-        //     } catch (InterruptedException e) {
-        //         break;
-        //     }
-        // }
+    public void stopEating() {
+        isEating = false;
     }
 
     public void takeDamage(int damage) {
         health -= damage;
-        if (health <= 0) alive = false;
+        if (health <= 0) {
+           // die();
+        }
     }
 
-    /*public boolean isAlive() {
-        return alive;
-    }*/
+//    private void die() {
+//        // Remove from game
+//        Game.getInstance().removeZombie(this);
+//    }
 
+    // Getters
+    public ImageView getView() { return view; }
     public int getRow() { return row; }
-    public int getCol() { return col; }
-    public void setCol(int col) { this.col = col; }
+    public double getColumn() { return column; }
 }
