1e104d91c723ca9acb81ddb88f0a91159f23e2e7
setayesh nejadmohammad <105065614+setayesh-nejadmohammad@users.noreply.github.com>
2025-06-30 10:56:43 +0330

    Update Plant.java

--- src/main/java/game/plantsvszambies/Plant.java
+++ src/main/java/game/plantsvszambies/Plant.java
@@ -1,20 +1,26 @@
 package game.plantsvszambies;
 
 
+import javafx.scene.image.ImageView;
+
 public abstract class Plant {
     protected int row;
     protected int col;
     protected int health;
     protected int cost;
     protected int rechargeTime;
+    protected ImageView view;
 
-    public Plant(int row, int col, int health, int cost, int rechargeTime) {
+    public Plant(int row, int col, int health, int cost, int rechargeTime, ImageView view) {
         this.row = row;
         this.col = col;
         this.health = health;
         this.cost = cost;
         this.rechargeTime = rechargeTime;
+        this.view = view;
     }
+    abstract void update(double deltaTime);
+
 
     //public abstract void act(Map map); // حرکت/شلیک یا تولید خورشید
 
@@ -24,6 +30,15 @@ public abstract class Plant {
 
     public boolean isDead() {
         return health <= 0;
+    }
+    public double getX() {
+
+        return col * 80 + 300;
+
+    }
+    public double getY() {
+
+        return row * 90 + 97;
     }
 
     public int getRow() { return row; }
