426a61ee08941c08dbd256813a322022a0e26a14
setayesh nejadmohammad <105065614+setayesh-nejadmohammad@users.noreply.github.com>
2025-07-10 00:09:53 +0330

    add snowpea and repeater

---
+++ src/main/java/game/plantsvszambies/SnowPea.java
@@ -0,0 +1,28 @@
+package game.plantsvszambies;
+
+import javafx.scene.image.ImageView;
+
+public class SnowPea extends ShooterPlant {
+    public static final double SLOW_DURATION = 3.0; // 3 seconds slow effect
+    public static final double SLOW_FACTOR = 0.5;
+
+    public SnowPea(int row, int col,  ImageView imageView) {
+        super(row, col, 5, 175, 7, imageView);
+        this.fireRate = 1;
+        this.damage = 15;
+        this.isSnowPea = true;
+    }
+    protected void shoot() {
+        Bullet bullet = new Bullet(
+                this.getX() + 10,     // Start slightly ahead of plant
+                this.getY(),     // Center vertically
+                this.getRow(),
+                damage,
+                300.0,                // Pixel per second speed
+                isSnowPea
+        );
+
+        Game.getInstance().addBullet(bullet);
+    }
+
+}
